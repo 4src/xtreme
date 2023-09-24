46e3e3d26a450ea69cdb506805b784de0261e754
Tim Menzies <timm@users.noreply.github.com>
2023-09-24 11:34:44 -0400

    saving

--- snap.py
+++ snap.py
@@ -24,17 +24,17 @@ from math import cos,log
 #  (_|  |  (_)  |_)  (_|  |  _> 
 #   _|                          
 
-the=dict(**{m[1]:scan(m[2]) for m in re.finditer(r"\n\s*-\w+\s*--(\w+).*=\s*(\S+)",__doc__)})
-
 BIG=1E30
-class obj: __repr__ = lambda i: printd(i.__dict__, i.__class__.__name__)
+class obj:       __repr__= lambda i: printd(i.__dict__, i.__class__.__name__)
+class box(dict): __repr__= lambda i:printd(i); __setattr__=dict.__setitem__; __getattr__=dict.get
+
 #----------------------------------------------------------------------------------------
 #   _   _   | 
 #  (_  (_)  |
 
 class COL(obj):
   def __init__(i,at=0,txt=" "): i.n,i.at,i.txt = 0,at,txt
-  def adds(i, a)  /: [i.add(x) for x in a]; return i
+  def adds(i, a)  : [i.add(x) for x in a]; return i
   def add(i,x)    :
     if x != "?": i.n += 1; i.add1(x)
   def dist(i,x,y) : return 1 if x=="?" and y=="?" else i.dist1(x,y)
@@ -46,16 +46,16 @@ class SYM(COL):
     i.has={}
     i.adds(l)
 
-  def bin1(x)    : return x
-  def add1(i,x)  : i.has[x] = 1 + i.has.get(x,0) 
-  def mid(x)     :  return mode(i.has)
-  def div(x)     :  return ent(i.has.values())
+  def bin1(x)     : return x
+  def add1(i,x)   : i.has[x] = 1 + i.has.get(x,0) 
+  def mid(x)      : return mode(i.has)
+  def div(x)      : return ent(i.has)
   def dist1(i,x,y): return 0 if x==y else 1
 
 class NUM(COL):
   def __init__(i, l=[], **kw): 
     super().__init__(**kw)
-    i._has,i.ok,i.w = {},True,0 if i.txt[-1]=="-" else 1
+    i._has,i.ok,i.heaven = {},True,0 if i.txt[-1]=="-" else 1
     i.adds(l)
 
   def add1(i,x)  : i.ok=False; i.has += [x]
@@ -90,31 +90,32 @@ class NUM(COL):
       9: [ -1.22,	-.76,	-.43,	-.14,	 .14,	 .43,  .76,	1.22],
      10: [ -1.28,	-.84,	-.52,	-.25,	   0,	 .25,  .52,	 .84,	1.28]}
 
-def mode(sym)      : return max(sym, key=sym.get)
-def ent(a)         : N = sum(a); return -sum(n/N*log(n/N,2) for n in a if n>0)
-def stdev(num)     : return (per(num,.9) - per(num,.1))/2.56
-def median(num)    : return per(num,.5)
-def per(num,n=0.5) : return num[int(n*len(num))]
+def ent(d)       : a = d.values(); N = sum(a); return -sum(n/N*log(n/N,2) for n in a if n>0)
+def mode(d)      : return max(d, key=sym.get)
+def mean(a)      : return sum(a)/len(a)
+def stdev(a)     : return (per(a,.9) - per(a,.1))/2.56
+def median(a)    : return per(a,.5)
+def per(a,n=0.5) : return a[int(n*len(a))]
 #-----------------------------------------------------------------------
 #  ._   _        
 #  |   (_)  \/\/ 
 
-class ROW(obj)
+class ROW(obj):
   def __init__(i,a)     : i.cost, i._data, i.cells, i.bins = 0, None, a, a[:]
   def __gt__(row1,row2) : return row1.d2h() < row2.d2h()
   def around(i,rows)    : return sorted(rows, key=lambda j: i.dist(j))
   def d2h(i):
     i.cost, d, m = 1, 0, 0
     for col in i._data.cols.y:
-      d += abs(x - col.w) ** 2
       m += 1
+      d += abs(i.cells[colt.at] - col.heaven) ** 2
     return (d/m) ** .5
   
   def dist(i,j):
     m,d = 0,0
     for col in i._data.cols.x:
       m += 1
-      d += col.dist(i.cells[n], j.cells[n]) ** the.bins
+      d += col.dist(i.cells[col.at], j.cells[col.at]) ** the.bins
     return (d/m) ** (1/the.bins)
 #----------------------------------------------------------------------------------------
 #   _|   _.  _|_   _. 
@@ -122,9 +123,17 @@ class ROW(obj)
 
 class DATA(obj):
   def __init__(i,src):
-    i.cols, i.rows = None, biglist()
+    i.cols, i.rows = None, []
     for row in src: i.add(row)
     return i.discretized()
+
+  def add(di,row):
+    if not i.cols: 
+      i.cols = COLS(row.cells)
+    else:
+      row._data = row._data or i
+      i.rows += [row]
+      [col.add(x) for col,x in zip(i.cols.all, row.cells)]
 
   def clone(i,rows=[]):
     return DATA([ROW(i.cols.names)] + rows)
@@ -138,124 +147,125 @@ class DATA(obj):
       row.bins[col.at] = col.bin(row.cells[col.at]
     return i
   
-class COLS(obj):
-  def __init__(i,a):
-    i.all    = [(NUM if s[0].isupper() else SYM)(at=n,txt=s) for n,s in enumerate(a)] 
-    i.x, i.y = {},{}
-    for n,(name,col) in enumerate(zip(a, i.all)):
-      if name[-1] != "X":
-        if name[-1] == "!": i.klass = col
-        (i.y if name[-1] in "!+-" else i.x).append(col)
+def COLS(names):
+  all,x,y,klass  = [],[],[],None
+  for n,s in enumerate(names):
+    a,z  = s[0], s[-1]       
+    col  = (NUM if a.isupper() else SYM)(at=n,txt=s)
+    all += [col]
+    if z != "X":
+      if z == "!": klass = col
+      (y if z in "!+-" else x).append(col)
+  return box(names=a, all=all, x=x, y=y, klass=klass)
 
-  def add(di,row):
-    if not i.cols: 
-      i.cols = COLS(row.cells)
-    else:
-      row._data = row._data or i
-      i.rows += [row]
-      [col.add(x) for col,x in zip(i.cols.all, row.cells)]
-#----------------------------------------------------------------------------------------
+ #----------------------------------------------------------------------------------------
 #   _  |        _  _|_   _   ._ 
 #  (_  |  |_|  _>   |_  (/_  |  
 
-def NODE(data,lvl=0): return box(data=data, lvl=lvl, left=None, right=None)
+class NODE(obj):
+  def __init__(i,data,lvl=0): i.data, i.lvl, i.left, i.right = data,lvl,None,None
 
-def tree(data0,sorting=False):
-  stop = len(data0.rows) ** the.min
-  def _grow(data, lvl):
-    node = NODE(data, lvl)
-    if len(data.rows) >= 2*stop:
-       _,__,left,right = half(data.rows, sorting)
-       data.mid   = right[0]
-       node.left  = _grow(clone(data0, left),  lvl+1)
-       node.right = _grow(clone(data0, right), lvl+1)
-    return node
-  return _grow(data0, 0)
+  def branches(i,sorting=False):
+    def _branches(data, lvl):
+      node = NODE(data, lvl)
+      if len(data.rows) >= 2*stop:
+         _,__,left,right = i.half(data.rows, sorting)
+         data.mid   = right[0]
+         node.left  = _branches(i.clone(left),  lvl+1)
+         node.right = _branches(i.clone(right), lvl+1)
+      return node
+    #------------
+    stop = len(i.rows) ** the.min
+    return _branches(i, 0)
+  
+  def branch(i):
+    def _branch(rows):
+      if len(rows) >= 2*stop:
+        _,__,left,right = i.half(rows, True)
+        rest.extend(right)
+        return _branch(left)
+      return rows,rest
+    #-----------------
+    stop = len(i.rows)**the.min
+    rest = []
+    return _branch(i.rows)
+  
+  def half(i,rows,sorting=False):
+    a,b,C = i.extremes( random.sample(rows, k=min(len(rows),the.Half)))
+    if sorting and b > a: a,b = b,a 
+    rows = sorted(rows, key=lambda r: (r.dist(a)**2 + C**2 - r.dist(b)**2)/(2*C))
+    mid  = int(len(rows)/2)
+    return a, b, rows[:mid], rows[mid:]
+  
+  def extremes(i,rows):
+    n = int(len(rows)*the.Far)
+    w = random.choice(rows)
+    x = around(w, rows)[n]
+    y = around(x, rows)[n]
+    return x,y, x.dist(y)
 
-def branch(data):
-  stop = len(data.rows)**the.min
-  rest = []
-  def _branch(rows):
-    if len(rows) >= 2*stop:
-      _,__,left,right = half(rows, True)
-      rest.extend(right)
-      return _branch(left)
-    return rows,rest
-  return _branch(data.rows)
+  def nodes(i,lvl=0):
+    yield i, not(i.left or i.right)
+    for kid in [i.left, i.right]:
+      for a,b in kid.nodes(lvl+1):
+        yield a,b
 
-def half(rows,sorting=False):
-  a,b,C = half_extremes( random.sample(rows, k=min(len(rows),the.Half)))
-  if sorting and better(b,a): a,b = b,a 
-  rows = sorted(rows, key=lambda r: (dist(r,a)**2 + C**2 - dist(r,b)**2)/(2*C))
-  mid  = int(len(rows)/2)
-  return a, b, rows[:mid], rows[mid:]
-
-def half_extremes(rows):
-  n = int(len(rows)*the.Far)
-  w = random.choice(rows)
-  x = around(w, rows)[n]
-  y = around(x, rows)[n]
-  return x,y, dist(x,y)
+  def show(i):
+    width = 4 * int(log(len(i.data.rows)**the.min,2))
+    for node1,leafp in i.nodes():
+      pre = '|.. ' *node1.lvl
+      if node1.lvl>0 and not leafp: 
+        print(f"{pre:{width}}")
+      else:
+        about = node1.data.stats()
+        if leafp: 
+          prints(f"{pre:{width}}", *about.values())
+        elif node.lvl==0:
+          prints(f"{' ':{width}}", *about.keys())
+          prints(f"{' ':{width}}", *about.values(),"mid")
+          prints(f"{' ':{width}}", *node1.data.stats(want="div").values(),"div")
 #----------------------------------------------------------------------------------------
 def prune(node0):
-  prune_status(node0,True)
-  rows = rows1 = node0.data.rows
-  while len(rows1) > len(rows) ** the.min:
-    e = prune_ents(rows1,{})
-    for one in sorted([node for node,_ in visit(node0) if prune_isSubtree(node)],
-                      key=lambda node1: -1 * prune_score(e,node1)]):
+  def _setStatus(node, status=True):
+    if node:
+      node.alive = status
+      for row in node.data.rows: row.alive=status
+      _setStatus(node.left,  status)
+      _setStatus(node.rleft, status)
+
+  def _bestLiveRoots():
+    return sorted([node for node,_ in node0.nodes() if _liveRoot(node)],
+                  reversed=True, key=lambda node1:  _score(e,node1)])
+
+  def _liveRoot(node):
+    n=node; return n.alive and n.left and n.left.alive and n.right and n.right.alive
+  
+  def _score(e,node):
+    a, b  = node.left.mid, node.right.mid
+    diffs = [col for col in node0.data.cols.x if a.bins[col.at] != b.bins[col.at]]
+    return sum(e[col.at]/(node.lvl+1) for col in diffs) / len(diffs)
+   
+  def _ents(rows):
+    return {c.at: ent(adds(SYM([r.bins[c.at] for r in rows if r.alive]))) for c in node0.data.cols.x}
+  _setStatus(node0, True)
+
+  b4 = now = node0.data.rows
+  stop = len(node0.data.rows) ** the.min
+  while True:
+    e = _ents(now)
+    for one in _liveRoots(): 
       if d2h(one.left.mid) != d2h(one.right.mid):
-        prune_status(one.right if better(one.left.mid,  one.right.mid) else one.left, False)
-        break
-    tmp = [row for row in rows1 if row.alive] 
-    if len(tmp) == len(rows1): print(2); break
-    rows1 = tmp
-    print(len(rows1))
-  return rows1
-
-def prune_score(e,node):
-  a, b  = node.left.mid, node.right.mid
-  diffs = [n for n in node0.data.cols.x.keys() if a.bins[n] != b.bins[n]]
-  return sum(e[n]/(node.lvl+1) for n in diffs) / len(diffs)
-
-def prune_isSubtree(node):
-  n=node; return n.alive and n.left and n.left.alive and n.right and n.right.alive
-
-def prune_status(node, status=True):
-  if node:
-    node.alive=status
-    for row in nodes.rows: row.alive=status
-    prune_status(node.left,  status)
-    prune_status(node.right, status)
-
-def prune_ents(rows,out):
-  return {n: ent( adds(SYM(), [r.bins[n] for r in rows if r.alive]).values()) 
-          for n in node0.data.cols.x.keys()}
-#----------------------------------------------------------------------------------------
+        _setStatus(one.right if one.left.mid > one.right.mid else one.left, False)
+        b4  = now 
+        now = [row for row in b4 if row.alive] 
+        if len(now) >= len(b4) or len(now) <= stop : return now
+        else: break
+  return now
+#-------------------------------------------------------------------------------------------------
 #      o   _  o  _|_ 
 #  \/  |  _>  |   |_ 
 
-def visit(node,lvl=0):
-  if node:
-    yield node, not(node.left or node.right)
-    for kid in [node.left, node.right]:
-      for a,b in visit(kid,lvl+1):
-        yield a,b
- 
-def show(node):
-  width = 4 * int(log(len(node.data.rows)**the.min,2))
-  for node1,leafp in visit(node):
-    pre = '|.. ' *node1.lvl
-    if node.lvl>0 and not leafp: 
-      print(f"{pre:{width}}")
-    else:
-      about = stats(node1.data)
-      if leafp: 
-        prints(f"{pre:{width}}", *about.values())
-      elif node.lvl==0:
-        prints(f"{' ':{width}}", *about.keys())
-        prints(f"{' ':{width}}", *about.values(),"mid")
-        prints(f"{' ':{width}}", *stats(node1.data, want=div).values(),"div")
+
 #----------------------------------------------------------------------------------------
 #   _  _|_  ._  o  ._    _    _ 
 #  _>   |_  |   |  | |  (_|  _> 
@@ -285,80 +295,80 @@ def prin(x,decimals=None):
   if decimals is None or not isinstance(x,float): return x
   return round(x,decimals)
 #----------------------------------------------------------------------------------------
-#   _  |   _.   _   _   _    _ 
-#  (_  |  (_|  _>  _>  (/_  _> 
+#   _   _   ._   _|_  ._   _   | 
+#  (_  (_)  | |   |_  |   (_)  | 
 
-class box(dict): 
-  __setattr__ = dict.__setitem__
-  __getattr__ = dict.get
-  __repr__    = printd
+class CONTROL(obj):
 
-class biglist(list):
-  def __repr__(i): 
-    return str(i) if len(i)<16 else str(i[:8])+".."+str(i[-8:])
+  def __init__(i,s):
+    d = {m[1]:coerce(m[2]) for m in re.finditer(r"\n\s*-\w+\s*--(\w+).*=\s*(\S+)",__doc__)}
+    i.__dict__.update(**d)
+
+  def cli(i):
+    d=i.__dict__
+    for k, v in d.items():
+      s = str(v)
+      for j, x in enumerate(sys.argv):
+        if ("-"+k[0])==x or ("--"+k)==x:
+          d[k] = coerce("True" if s=="False" else ("False" if s=="True" else sys.argv[j+1]))
+
+  def run(i,name,fun):
+    d=i.__dict__
+    b4=deepcopy(d)
+    random.seed(the.seed)
+    if bad := fun()==False: print(f"❌  FAIL : {name}") 
+    for k in b4: d[k] = b4[k]
+    return bad
+  
+  def main(i):
+    i.cli()
+    if i.help: print(__doc__)
+    [i.run(x,funs[x]) for x in sys.argv if x in EGS.egs]
 #----------------------------------------------------------------------------------------
-#  ._ _    _.  o  ._  
-#  | | |  (_|  |  | | 
+#   _    _    _ 
+#  (/_  (_|  _> 
+#        _|     
 
-def cli(d):
-  for k, v in d.items():
-    s = str(v)
-    for j, x in enumerate(sys.argv):
-      if ("-"+k[0])==x or ("--"+k)==x:
-        d[k] = coerce("True" if s=="False" else ("False" if s=="True" else sys.argv[j+1]))
-  return d
+class EGS:
+  egs= {k:v for k,v in locals().items() if callable(v) and k[0].islower()}
 
-def run(name,fun,funs):
-  b4=deepcopy(the)
-  random.seed(the.seed)
-  if bad := fun(funs)==False: print(f"❌  FAIL : {name}") 
-  for k in b4: the[k] = b4[k]
-  return bad
+  def all():
+    sys.exit(sum([the.run(k,fun) for k,fun in EGS.egs.items() if k != "all"]) - 1)
+  
+  def fail_what_happens_when_we_fail(_):  return 1 > 2
 
-def main(funs):
-  cli(the)
-  funs = {k[5:]:v for k,v in funs.items() if k[:5]=="test_"}
-  [run(x,funs[x],funs) for x in sys.argv if x in funs]
-#----------------------------------------------------------------------------------------
-#  _|_   _    _  _|_   _ 
-#   |_  (/_  _>   |_  _> 
+  def the(_):   print(the)
 
-def test_all(tests):
-  sys.exit(sum([run(k,fun,tests) for k,fun in tests.items() if k != "all"]) - 1)
-
-def test_fail_what_happens_when_we_fail(_):  return 1 > 2
-def test_the(_):   print(the)
-def test_stats(_): printds(stats(DATA(csv(the.file))))
-
-def test_dist(_): 
-  d=DATA(csv(the.file))
-  rows=around(d.rows[0], d.rows)
-  for i in range(0,len(rows),30):
-    print(i, dist(rows[0],rows[i]))
-
-def test_half(_): 
-  d = DATA(csv(the.file))
-  a,b,c,d= half(d.rows)
-  print(len(c),len(d))
-
-def test_tree(_): 
-  d = DATA(csv(the.file))
-  show(tree(d,sorting=True))
-
-def test_sort(_):
-  d = DATA(csv(the.file))
-  rows = sorted(d.rows, key=d2h)
-  print(stats(clone(d, rows[:50])))
-  print(stats(clone(d, rows[-50:])))
-
-def test_branch(_):
-  d = DATA(csv(the.file))
-  best,rest= branch(d)
-  print(stats(clone(d, best)))
-  print(stats(clone(d, rest)))
+  def stats(_): printds(DATA(csv(the.file)).stats())
+  
+  def dist(_): 
+    d=DATA(csv(the.file))
+    rows=d.around(d.rows[0], d.rows)
+    for i in range(0,len(rows),30):
+      print(i, rows[0].dist(rows[i]))
+  
+  def half(_): 
+    d = DATA(csv(the.file))
+    a,b,k1,k2= d.half(d.rows)
+    print(len(k1),len(k2))
+  
+  def branches(_): 
+    d = DATA(csv(the.file))
+    show(d.branches(sorting=True))
+  
+  def sort(_):
+    d = DATA(csv(the.file))
+    rows = sorted(d.rows, key=lambda row: row.d2h())
+    print(d.clone(rows[:50]).stats())
+    print(d.clone(rows[-50:]).stats())
+  
+  def branch(_):
+    d = DATA(csv(the.file))
+    best,rest= d.branch()
+    print(d.clone(best).stats())
+    print(d.clone(rest).stats())
 #----------------------------------------------------------------------------------------
 #   _  _|_   _.  ._  _|_ 
 #  _>   |_  (_|  |    |_ 
 
-the=box(**the)
-if __name__ == "__main__": main(locals())
+if __name__ == "__main__": the=CONTROL(__doc__); the.main()
